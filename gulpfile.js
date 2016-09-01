b94dd86c1981f59b393659a65b51dc5d93bdc815
LLTommy <Thomas.Liener@felix-solution.com>
2016-09-01 14:31:40 +0100

    Pushing the new widget for the first time

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+
+gulp.task('uglify', function(){
+ console.log("This is the task uglify from gulp");
+ return gulp.src('build/ols-autocomplete.js')
+ .pipe(uglify())
+ //.pipe(rename({extname: '.min.js'}))
+ .pipe(gulp.dest('build'));
+})
